1c8c272ea8c08d6651d2baf09f26b8b804706a2c
karimmufti <karroomdj2005@gmail.com>
2025-07-16 17:26:52 -0500

    Initial Commit

---
+++ Flight_Board/Flight_BoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flight_BoardApp.swift
+//  Flight_Board
+//
+//  Created by Karim Mufti on 7/16/25.
+//
+
+import SwiftUI
+
+@main
+struct Flight_BoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
